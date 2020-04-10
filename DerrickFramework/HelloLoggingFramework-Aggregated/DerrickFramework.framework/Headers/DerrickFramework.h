c386d0e25de8e4ffbb24d13d11a05dc046a54a82
Derrick <kang.derrick@gmail.com>
2020-04-10 15:46:24 +0900

    completed basic...s

--- DerrickFramework/HelloLoggingFramework-Aggregated/DerrickFramework.framework/Headers/DerrickFramework.h
+++ DerrickFramework/HelloLoggingFramework-Aggregated/DerrickFramework.framework/Headers/DerrickFramework.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char DerrickFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DerrickFramework/PublicHeader.h>
 
+#import <DerrickFramework/DerrickTest.h>
+
 
